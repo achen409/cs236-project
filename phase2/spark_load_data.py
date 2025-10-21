070df4fe32317b0e3c56ffdf91452dc61ee6c288
Andy Chen <andrewr.chen@gmail.com>
2025-10-21 12:50:59 -0700

    loading all three dbs

--- phase2/spark_load_data.py
+++ phase2/spark_load_data.py
@@ -1,21 +1,44 @@
 from pyspark.sql import SparkSession
 
+# spark session init
 spark: SparkSession = SparkSession.builder.config(
     "spark.jars.packages", "org.postgresql:postgresql:42.7.8"
 ).getOrCreate()
 
-bookings_df = spark.read.csv(
+# load CSV files
+customer_reservations = spark.read.csv(
+    "customer_reservations.csv", header=True, inferSchema=True
+)
+hotel_booking_cleaned = spark.read.csv(
+    "hotel_booking_cleaned.csv", header=True, inferSchema=True
+)
+all_bookings = spark.read.csv(
     "phase1_output_datasets/all_bookings.csv",
     header=True,
     inferSchema=True,
 )
 
-bookings_df.write.jdbc(
-    "jdbc:postgresql://localhost:5432/bookings",
-    "bookings",
-    properties={
-        "user": "postgres",
-        "password": "password",
-        "driver": "org.postgresql.Driver",
-    },
+# spark db authentication info
+spark_db_url = "jdbc:postgresql://localhost:5432/bookings"
+spark_db_properties = {
+    "user": "postgres",
+    "password": "password",
+    "driver": "org.postgresql.Driver",
+}
+
+# writing to postgres db
+customer_reservations.write.jdbc(
+    spark_db_url,
+    "customer_reservations",
+    properties = spark_db_properties,
+)
+hotel_booking_cleaned.write.jdbc(
+    spark_db_url,
+    "hotel_booking",
+    properties = spark_db_properties,
+)
+all_bookings.write.jdbc(
+    spark_db_url,
+    "all_bookings",
+    properties = spark_db_properties,
 )
