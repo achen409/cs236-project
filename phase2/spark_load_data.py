57e92e591ff1a27ce5d7bc6e3315a4a7531af0bb
W2YUCR <146299962+W2YUCR@users.noreply.github.com>
2025-10-23 06:28:21 -0700

    Add SQL schema and data loading

--- phase2/spark_load_data.py
+++ phase2/spark_load_data.py
@@ -75,19 +75,19 @@ spark_db_properties = {
 }
 
 # writing to postgres db
-customer_reservations.write.jdbc(
+customer_reservations.write.option("truncate", "true").jdbc(
     spark_db_url,
     "customer_reservations",
     properties = spark_db_properties,
     mode="overwrite"
 )
-hotel_booking.write.jdbc(
+hotel_booking.write.option("truncate", "true").jdbc(
     spark_db_url,
     "hotel_booking",
     properties = spark_db_properties,
     mode="overwrite"
 )
-all_bookings.write.jdbc(
+all_bookings.write.option("truncate", "true").jdbc(
     spark_db_url,
     "all_bookings",
     properties = spark_db_properties,
